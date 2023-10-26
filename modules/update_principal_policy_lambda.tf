63430bf611d260f22fc6485c18ff0be19ff13272
Jimmy Huang <jimmy.huang@observeinc.com>
2023-10-25 19:54:09 -0700

    updated role max session duration from 4 hours to 8 hours

--- modules/update_principal_policy_lambda.tf
+++ modules/update_principal_policy_lambda.tf
@@ -21,7 +21,7 @@ module "update_principal_policy" {
     PRINCIPAL_POLICY_S3_KEY        = aws_s3_bucket_object.principal_policy.key
     PRINCIPAL_IAM_DENY_TAGS        = join(",", var.principal_iam_deny_tags)
     ALLOWED_REGIONS                = join(",", var.allowed_regions)
-    PRINCIPAL_MAX_SESSION_DURATION = 14400
+    PRINCIPAL_MAX_SESSION_DURATION = 28800 // Original Default = 4 hours (14400 seconds)
     TAG_ENVIRONMENT                = var.namespace == "prod" ? "PROD" : "NON-PROD"
     TAG_APP_NAME                   = lookup(var.global_tags, "AppName")
   }
